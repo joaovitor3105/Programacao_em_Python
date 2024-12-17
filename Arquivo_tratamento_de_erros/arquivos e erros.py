4505bf0dba6c25b8b9da62a34961417f5a62ce08
João Viltor Lobato <joaovitorlobatoromualdo16@gmail.com>
2024-12-17 17:06:38 -0300

    update

---
+++ Arquivo_tratamento_de_erros/arquivos e erros.py
@@ -0,0 +1,71 @@
+while True:
+    print("MENU\n1-Criar arquivo\n2-Mostrar arquivo\n3-Editar arquivo\n4-Sair do programa")
+    
+    
+    try:
+        num=int(input("\nDigite uma opcao:"))
+    except ValueError:
+        continue
+    match num:
+        case 1:
+            nome=input("\nDigite o nome do arquivo:")
+            with open (nome+".txt",'w') as file :
+                x=input('\nDeseja adicionar escrita(sim ou nao)?')
+                if x == 'sim':
+                    file.write(input('\nDigite o que deseja adicionar ao arquivo:\n'))
+                else :
+                    pass
+            
+        case 2 :
+            nome=input('\nQual o nome do arquivo?')
+            try:
+                with open (nome+'.txt','r')  as file:
+                    conteudo=file.read()
+                    print(conteudo)
+            except FileNotFoundError:
+                with open ('gerenciamento_erros.log','w') as log :
+                    log.write("\nErro ao tentar abrir arquivo,não existente\n")
+                print('\nArquivo não existente')
+                pass
+            
+        case 3 :
+            try:
+                num=int(input("\nQual opção desejada :\n 1-Abrir arquivo para reescrita \n 2-Adicionar escrita ao arquivo"))
+            except ValueError:
+                continue
+            if num == 1 :
+                
+                try:
+                    nome=input('\nQual o nome do arquivo?')
+                    with open (nome+'.txt','w')  as file:
+                        file.write(input('\nDigite o que deseja adicionar ao arquivo:\n'))
+
+                except FileNotFoundError:
+                    with open ('gerenciamento_erros.log','a') as log :
+                        log.write("\nErro ao tentar abrir arquivo para editar,não existente\n")
+                    print('\nArquivo não existente')
+                    
+            if num == 2 :
+                
+                try:
+                    nome=input('\nQual o nome do arquivo?')
+                    with open (nome+'.txt','a')  as file:
+                        
+                        file.write(input('\nDigite o que deseja adicionar ao arquivo:\n'))
+
+                except FileNotFoundError:
+                    with open ('gerenciamento_erros.log','a') as log :
+                        log.write("\nErro ao tentar abrir arquivo para editar,não existente\n")
+                    print('\nArquivo não existente')
+        case 4 :
+            print('Saindo do programa')
+            break
+                
+            
+            
+            
+            
+            
+                           
+                    
+                
