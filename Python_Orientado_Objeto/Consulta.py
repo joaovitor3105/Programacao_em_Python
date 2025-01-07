f32762fe7f7818f1cdb4a0d4a046b2f273dcfa32
joaovitor3105 <joaovitorlobatoromualdo16@gmail.com>
2025-01-07 17:12:09 -0300

    update

--- Python_Orientado_Objeto/Consulta.py
+++
@@ -1,9 +0,0 @@
-from Paciente import Paciente
-class Consulta():
-    def __init__(self,p,relato,medicamentos):
-        self.p=p
-        self.relato=relato
-        self.medicamentos=medicamentos
-    
-    
-    
--- Python_Orientado_Objeto/Paciente.py
+++
@@ -1,7 +0,0 @@
-from Pessoa import Pessoa
-class Paciente(Pessoa):
-    def __init__(self, nome, sexo, endereco, cpf, telefone, identidade,medicacaocontinua):
-        super().__init__(nome, sexo, endereco, cpf, telefone, identidade)
-        self.medicacaocontinua=medicacaocontinua
-    
-    
