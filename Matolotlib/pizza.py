449cbeb969abf7c83a402f1c99ae2d340164b00b
joaovitor3105 <joaovitorlobatoromualdo16@gmail.com>
2025-01-07 17:28:38 -0300

    add

--- Matolotlib/pizza.py
+++ Matolotlib/pizza.py
@@ -1,4 +1,5 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 pizza = ["cheese","pepperoni","sausage","mushroom","onion","pepper","anchovy","olive","pineapple","ham","spinach","artichoke","anchovy","pepper","onion","mushroom","sausage","pepperoni","cheese"]
-plt.pie(labels=pizza,explode=True,autopct='%1.1f%%')
+sizes = [1]*len(pizza)
+plt.pie(sizes,labels=pizza,autopct='%1.1f%%')
 plt.show()
